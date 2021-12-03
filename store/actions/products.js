631098b5b2515195377e4255e2c551987036b331
mariovelyov <mario.veliov@looming.tech>
2021-12-03 13:42:28 +0200

    update and delete product logic added to firebase

--- store/actions/products.js
+++ store/actions/products.js
@@ -44,9 +44,18 @@ export const fetchProducsts = () => {
 };
 
 export const deleteProduct = (productId) => {
-  return {
-    type: DELETE_PRODUCT,
-    pid: productId,
+  return async (dispatch) => {
+    await fetch(
+      `https://rn-complete-guide-65759-default-rtdb.firebaseio.com/products/${productId}.json`,
+      {
+        method: "DELETE",
+      }
+    );
+
+    dispatch({
+      type: DELETE_PRODUCT,
+      pid: productId,
+    });
   };
 };
 
@@ -84,13 +93,30 @@ export const createProduct = (title, description, imageUrl, price) => {
 };
 
 export const updateProduct = (id, title, description, imageUrl) => {
-  return {
-    type: UPDATE_PRODUCT,
-    pid: id,
-    productData: {
-      title,
-      description,
-      imageUrl,
-    },
+  return async (dispatch) => {
+    await fetch(
+      `https://rn-complete-guide-65759-default-rtdb.firebaseio.com/products/${id}.json`,
+      {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          title,
+          description,
+          imageUrl,
+        }),
+      }
+    );
+
+    dispatch({
+      type: UPDATE_PRODUCT,
+      pid: id,
+      productData: {
+        title,
+        description,
+        imageUrl,
+      },
+    });
   };
 };
